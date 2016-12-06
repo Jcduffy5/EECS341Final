87d8928efc03a77f9b0eef9ec9083003a8255254
John-Sailor <jrs297@case.edu>
2016-12-06 15:18:29 -0500

    modularized design

--- src/Query.java
+++ src/Query.java
@@ -3,5 +3,25 @@
  */
 public class Query {
 
+    public static void main(String args[]) {
+        Query test = new Query();
+        test.findProduct("Lights");
+
+
+    }
+    public void findProduct(String product){
+        String query = "SELECT product.prodName, r0.aisle, r0.bay, r0.shelf " +
+                "FROM product, r0 " +
+                "WHERE product.prodID= r0.prodID AND product.prodName LIKE '%"+product+"%'";
+        this.establishConnection(query);
+
+    }
+
+    public void establishConnection( String query){
+        targetConnection connection = new targetConnection();
+        connection.establishConnection();
+        connection.executeQuery(query);
+
+    }
 
 }
--- src/targetConnection.java
+++ src/targetConnection.java
@@ -2,20 +2,71 @@
  * Created by John on 12/5/2016.
  */
 import java.sql.*;
+import java.time.Clock;
+import java.util.ArrayList;
+import java.util.List;
 
 public class targetConnection {
+    Statement stmt;
+    ResultSet rs;
+    Connection con;
 
-        public static void main(String args[]){
-            try{
-                Class.forName("com.mysql.jdbc.Driver");
-                Connection con=DriverManager.getConnection(
-                        "jdbc:mysql://localhost:3306/target?autoReconnect=true&useSSL=false","root","jrgreen");
-//here sonoo is database name, root is username and password
-                Statement stmt=con.createStatement();
-                ResultSet rs=stmt.executeQuery("select product.prodName, product.price from product where product.price= 6.49");
-                while(rs.next())
-                    System.out.println(rs.getString(1)+"  "+rs.getString(2));
-                con.close();
-            }catch(Exception e){ System.out.println(e);}
+    public static void main(String args[]) {
+
+
+
+    }
+
+    public void establishConnection() {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            con = DriverManager.getConnection(
+                    "jdbc:mysql://localhost:3306/target?autoReconnect=true&useSSL=false", "root", "jrgreen");
+
+
+
+        } catch (Exception e) {
+            System.out.println(e);
         }
     }
+
+    public void executeQuery(String query) {
+        try {
+            stmt = con.createStatement();
+            rs = stmt.executeQuery(query);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        this.processResults();
+
+
+    }
+
+    public Object[] processResults() {
+        int nCol;
+        List<String[]> table = new ArrayList<>();
+        try {
+            nCol = rs.getMetaData().getColumnCount();
+            while (rs.next()) {
+                String[] row = new String[nCol];
+                for (int iCol = 0; iCol < nCol; iCol++) {
+                    Object obj = rs.getObject(iCol + 1);
+                    row[iCol] = (obj == null) ? null : obj.toString();
+                }
+                table.add(row);
+                for( String s : row){
+                    System.out.print(s+"  ");
+                }
+                System.out.println();
+            }
+            con.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return table.toArray();
+
+
+
+
+    }
+}

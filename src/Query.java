cd702f2e7247b02108c7104527e002816ecc79bf
John-Sailor <jrs297@case.edu>
2016-12-06 16:10:05 -0500

    modularized design

--- src/Query.java
+++ src/Query.java
@@ -6,6 +6,7 @@ public class Query {
     public static void main(String args[]) {
         Query test = new Query();
         test.findProduct("Lights");
+        test.compareProduct("lights");
 
 
     }
@@ -16,6 +17,36 @@ public class Query {
         this.establishConnection(query);
 
     }
+
+    public void compareProduct(String prod1, String prod2){
+        String query = "SELECT product.prodName, product.specs " +
+                "FROM product " +
+                "WHERE product.prodName LIKE '%"+prod1+"%' OR product.prodName LIKE '%"+prod1+"%'";
+        this.establishConnection(query);
+    }
+
+    public void findEmpProd(String product){
+        String query = "SELECT employee.empName, employee.phone" +
+                "FROM product, r0, employee " +
+                "WHERE product.prodID= r0.prodID AND r0.empID = employee.empID AND employee.onClock = '1' AND product.prodName LIKE '%"+product+"%'";
+        this.establishConnection(query);
+    }
+    public void inStock(String product, int zip ){
+        String query = "SELECT product.prodName, store.StoreName, r0.aisle, r0.bay, r0.shelf " +
+                "FROM product, r0, store" +
+                "WHERE product.prodID= r0.prodID AND r0.storeID = store.storeID AND store.zipCode ="+zip+" product.prodName LIKE '%"+product+"%'";
+        this.establishConnection(query);
+    }
+
+    public void deptSearch( String dept){
+        String query = "SELECT department.departmentName,r0.aisle, r0.bay" +
+                "FROM  r0, department " +
+                "WHERE  r0.deptID = department.deptID AND department.deptName LIKE '%"+dept+"%'";
+        this.establishConnection(query);
+    }
+
+
+
 
     public void establishConnection( String query){
         targetConnection connection = new targetConnection();

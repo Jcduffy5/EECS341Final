fc23f7a9122bb7ede5cce97d09e79b3fb1742f13
John-Sailor <jrs297@case.edu>
2016-12-06 17:54:42 -0500

    modularized design

--- src/Query.java
+++ src/Query.java
@@ -6,7 +6,18 @@ public class Query {
     public static void main(String args[]) {
         Query test = new Query();
         test.findProduct("Lights");
-        test.compareProduct("lights");
+        System.out.println();
+        System.out.println();
+        test.compareProduct("lights", "timex");
+        System.out.println();
+        System.out.println();
+        test.findEmp();
+        System.out.println();
+        System.out.println();
+        test.inStock("battlefield",44109 );
+        System.out.println();
+        System.out.println();
+        test.deptSearch("electronics");
 
 
     }
@@ -21,25 +32,22 @@ public class Query {
     public void compareProduct(String prod1, String prod2){
         String query = "SELECT product.prodName, product.specs " +
                 "FROM product " +
-                "WHERE product.prodName LIKE '%"+prod1+"%' OR product.prodName LIKE '%"+prod1+"%'";
+                "WHERE product.prodName LIKE '%"+prod1+"%' OR product.prodName LIKE '%"+prod2+"%'";
         this.establishConnection(query);
     }
 
-    public void findEmpProd(String product){
-        String query = "SELECT employee.empName, employee.phone" +
-                "FROM product, r0, employee " +
-                "WHERE product.prodID= r0.prodID AND r0.empID = employee.empID AND employee.onClock = '1' AND product.prodName LIKE '%"+product+"%'";
+    public void findEmp(){
+        String query = "SELECT employee.empName, employee.phone FROM employee WHERE employee.onClock";
+
         this.establishConnection(query);
     }
     public void inStock(String product, int zip ){
-        String query = "SELECT product.prodName, store.StoreName, r0.aisle, r0.bay, r0.shelf " +
-                "FROM product, r0, store" +
-                "WHERE product.prodID= r0.prodID AND r0.storeID = store.storeID AND store.zipCode ="+zip+" product.prodName LIKE '%"+product+"%'";
+        String query = "SELECT product.prodName, store.storeName, store.street, store.city, store.state FROM product, r0, store WHERE product.prodID= r0.prodID AND r0.storeID = store.storeID AND store.zipCode = '"+zip+"' AND product.prodName LIKE '%"+product+"%'";
         this.establishConnection(query);
     }
 
     public void deptSearch( String dept){
-        String query = "SELECT department.departmentName,r0.aisle, r0.bay" +
+        String query = "SELECT department.deptName,r0.aisle, r0.bay " +
                 "FROM  r0, department " +
                 "WHERE  r0.deptID = department.deptID AND department.deptName LIKE '%"+dept+"%'";
         this.establishConnection(query);

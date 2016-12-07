878f06fcfedcd45155f36b6facb5d20e466c22ea
John-Sailor <jrs297@case.edu>
2016-12-06 22:01:42 -0500

    changed query

--- src/Query.java
+++ src/Query.java
@@ -14,11 +14,14 @@ public class Query {
 
     }
     public JtableHelper findProduct(String product){
+        Long startTime = System.nanoTime();
         String query = "SELECT product.prodName, r0.aisle, r0.bay, r0.shelf " +
                 "FROM product, r0 " +
                 "WHERE product.prodID= r0.prodID AND product.prodName LIKE '%"+product+"%'";
         String[] headers = {"Product Name", "Aisle", "Bay", "Shelf"};
+        System.out.println(System.nanoTime()-startTime);
                 return new JtableHelper(headers,this.establishConnection(query));
+
 
 
 
@@ -27,30 +30,38 @@ public class Query {
     }
 
     public JtableHelper compareProduct(String prod1, String prod2){
+        Long startTime = System.nanoTime();
         String query = "SELECT product.prodName, product.specs " +
                 "FROM product " +
                 "WHERE product.prodName LIKE '%"+prod1+"%' OR product.prodName LIKE '%"+prod2+"%'";
         String[] headers = {"Product Name", "Specifications"};
+        System.out.println(System.nanoTime()-startTime);
         return new JtableHelper(headers,this.establishConnection(query));
     }
 
     public JtableHelper findEmp(){
+        Long startTime = System.nanoTime();
         String query = "SELECT employee.empName, employee.phone FROM employee WHERE employee.onClock";
 
         String[] headers = {"Name", "Work Phone Number"};
+        System.out.println(System.nanoTime()-startTime);
         return new JtableHelper(headers,this.establishConnection(query));
     }
     public JtableHelper inStock(String product, int zip ){
+        Long startTime = System.nanoTime();
         String query = "SELECT product.prodName, store.storeName, store.street, store.city, store.state, store.ZipCode FROM product, r0, store WHERE product.prodID= r0.prodID AND r0.storeID = store.storeID AND store.zipCode = '"+zip+"' AND product.prodName LIKE '%"+product+"%'";
         String[] headers = {"Product Name", "Store", "Street", "City", "State", "Zip Code"};
+        System.out.println(System.nanoTime()-startTime);
         return new JtableHelper(headers,this.establishConnection(query));
     }
 
     public JtableHelper deptSearch( String dept){
+        Long startTime = System.nanoTime();
         String query = "SELECT department.deptName, product.prodName,r0.aisle, r0.bay " +
                 "FROM  r0, department, product " +
                 "WHERE  product.prodID = r0.prodID AND r0.deptID = department.deptID AND department.deptName LIKE '%"+dept+"%'";
         String[] headers = {"Department","Product Name","Aisle", "Bay"};
+        System.out.println(System.nanoTime()-startTime);
         return new JtableHelper(headers,this.establishConnection(query));
     }
 
